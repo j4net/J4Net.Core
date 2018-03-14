3aa9c00408fa94bdc2a98320f7617610b495cb63
Gregory <gnkoshelev@gmail.com>
2018-03-14 16:17:34 +0500

    Fix elusive bug with 0-terminal UTF-8 string.

--- J4Net.Core/Util/StringExtensions.cs
+++ J4Net.Core/Util/StringExtensions.cs
@@ -6,7 +6,10 @@ namespace J4Net.Core.Util
     {
         public static byte[] ToUtfBytes(this string str)
         {
-            return Encoding.UTF8.GetBytes(str);
+            int count = Encoding.UTF8.GetByteCount(str);
+            byte[] utf = new byte[count + 1];
+            Encoding.UTF8.GetBytes(str, 0, str.Length, utf, 0);
+            return utf;
         }
     }
 }
